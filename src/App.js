ca26baf2d9d09eaeaf5ba8fc58524d2b01e6d50d
kissyla <kissylamottapereira@gmail.com>
2020-11-13 19:01:38 -0300

    refatorando exercise-one

--- src/App.js
+++ src/App.js
@@ -1,11 +1,35 @@
-import React from 'react';
+import React, { createContext } from 'react';
 import './App.css';
 import Cars from './Cars';
 
-function App() {
-  return (
-    <Cars />
-  );
+export const CarContext = createContext();
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.moveCar = this.moveCar.bind(this);
+    this.state = {
+      red: false,
+      blue: false,
+      yellow: false,
+    }
+  }
+
+  moveCar (car, side) {
+    this.setState({
+      [car]: side,
+    })
+  }
+
+  render() {
+    return (
+      <CarContext.Provider value={{...this.state, moveCar: this.moveCar}}> 
+      {/* chaves de fora js em HTLM chaves de dentro é a chave do objeto */}
+      <Cars />
+      </CarContext.Provider>
+    );
+  }
+
 }
 
 export default App;
